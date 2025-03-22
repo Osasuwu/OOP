2599bef9dc09851d932566d749e6a91d343754d8
Osas_uwu <petrkudr2@gmail.com>
2025-03-22 23:01:30 +0500

    Revert "update gitignore"
    This reverts commit ddde70d04a6189903a8acccd526cbc3c361eea80.

---
+++ src/main/java/services/output/M3uPlaylistExporter.java
@@ -0,0 +1,27 @@
+package services.output;
+
+import models.Playlist;
+import models.Song;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class M3uPlaylistExporter implements PlaylistExporter {
+    @Override
+    public void export(Playlist playlist, String destination) {
+        try (FileWriter writer = new FileWriter(destination)) {
+            writer.append("#EXTM3U\n"); // M3U playlist header
+
+            for (Song song : playlist.getSongs()) {
+                writer.append("#EXTINF:").append(String.valueOf(song.getDuration())).append(",")
+                      .append(song.getArtist()).append(" - ").append(song.getTitle()).append("\n");
+                writer.append(song.getFilePath()).append("\n"); // Assuming Song has a file path
+            }
+
+            System.out.println("Playlist exported as M3U to: " + destination);
+        } catch (IOException e) {
+            System.err.println("Error exporting M3U: " + e.getMessage());
+        }
+    }
+}
+
